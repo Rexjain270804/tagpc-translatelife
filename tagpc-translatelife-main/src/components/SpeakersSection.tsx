4e509dc85fa1d3df02f3ba65a96b82393f3c28d1
Tapas Pandey <contact.tapas08@gmail.com>
2026-01-15 19:23:10 +0530

    Modify designation for Prof. Panchanan Mohanty
    Updated designation for Prof. Panchanan Mohanty.

--- tagpc-translatelife-main/src/components/SpeakersSection.tsx
+++ tagpc-translatelife-main/src/components/SpeakersSection.tsx
@@ -51,10 +51,10 @@ const SpeakersSection = () => {
       designation: "Council Member ICSSR, New Delhi. Professor (Retd.), Department of Political Science, University of Rajasthan",
       image: "/speakers/sheila_rai.png"
     },
-    {
+   {
       name: "Prof. Panchanan Mohanty",
       title: "Guest of Honour/Keynote/Plenary Speaker",
-      designation: "Council Member ICSSR, New Delhi. Professor (Retd.), Department of Political Science, University of Rajasthan",
+      designation: "Council Member ICSSR, New Delhi. Professor (Retd.), Center of Applied Linguistics and Translation studies, Maulana Azad National Urdu University and Malaviya National Institute of Technology",
       image: "/speakers/panchanan_mohanty.png"
     },
     {
